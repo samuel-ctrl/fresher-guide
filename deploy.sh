526d21353c5d6512c566044cb2fb1bd7723e01bf
samuel <samuel.softsuave@gmail.com>
2024-07-24 16:24:23 +0530

    init deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install MkDocs and the Material theme
+pip install mkdocs
+
+# Install http-server globally using npm
+npm install -g http-server
+
+# Build the MkDocs site
+mkdocs build
+
+# Start http-server to serve the site (optional, for local preview)
+http-server site
